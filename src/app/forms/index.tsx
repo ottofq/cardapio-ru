1911253c99fb23ffd32569c077f634e3630825cd
Otto F. Quintanilha <otto@mobilesaude.com.br>
2023-09-18 23:27:55 -0300

    refactor: intro form

--- src/app/forms/index.tsx
+++ src/app/forms/index.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'expo-router';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
+
+import Logo from '@/assets/images/icon.png';
 
 export default function Intro() {
   const router = useRouter();
@@ -10,16 +12,19 @@ export default function Intro() {
   };
 
   return (
-    <View className="flex-1 bg-white p-4">
-      <Text>Bem Vindo</Text>
-      <Text>
-        Antes de exibir o cardápio, precisamos que responda um questionário para
-        o setor de nutrição do RU CCA UFES.
-      </Text>
+    <View className="flex-1  bg-white p-4">
+      <View className="flex-1 items-center justify-center gap-1 p-4">
+        <Image source={Logo} className="mb-10 h-40 w-40 self-center rounded" />
+        <Text className="text-xl font-bold">Bem Vindo</Text>
+        <Text className=" text-center text-lg">
+          Antes de exibir o cardápio, preencha seus dados e responda um
+          questionário para o setor de nutrição do RU CCA UFES.
+        </Text>
+      </View>
 
       <View className="mt-auto pt-4">
         <Button className="mt-auto" mode="contained" onPress={navigate}>
-          PRÓXIMO
+          CADASTRAR
         </Button>
       </View>
     </View>
