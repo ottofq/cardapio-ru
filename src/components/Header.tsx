a67e5dc1c0dd958d37f322879a2a8a32526817fe
Otto F. Quintanilha <otto@mobilesaude.com.br>
2023-09-18 23:28:44 -0300

    feat: add login/logout text in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,29 @@
-import { MaterialIcons } from '@expo/vector-icons';
-import { View } from 'react-native';
+import { Link } from 'expo-router';
+import { Pressable, View } from 'react-native';
 import { Text } from 'react-native-paper';
 
+import useAuth from '@/hooks/useAuth';
+
 export default function Header() {
+  const { signOut, token } = useAuth();
+
   return (
     <View className="flex-row items-center bg-primary p-4">
-      <MaterialIcons name="menu" size={24} color="white" />
       <Text className="flex-1 text-center text-white" variant="titleLarge">
         CARDÁPIO RU
       </Text>
+
+      {token ? (
+        <Pressable onPress={signOut}>
+          <Text className="text-white">LOGOUT</Text>
+        </Pressable>
+      ) : (
+        <Link href="/login" asChild>
+          <Pressable>
+            <Text className="text-white">LOGIN</Text>
+          </Pressable>
+        </Link>
+      )}
     </View>
   );
 }
